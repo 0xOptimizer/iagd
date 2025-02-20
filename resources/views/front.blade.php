e40fe3196451e0d4f76ba2e2ec5b9889bda74bf6
ビビ <38562978+0xOptimizer@users.noreply.github.com>
2025-02-20 13:22:56 +0800

    update: landing

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -35,77 +35,129 @@
             </div>
 
             <!-- Right Group - Buttons -->
-            <div class="d-flex">
-            <button type="button" class="tewi-btn-icon me-2"><i class="bi bi-grid-3x3-gap-fill" style="vertical-align: 0px; font-size: 32px;"></i></button>
-            <button type="button" class="tewi-btn-icon me-2"><i class="bi bi-grid-3x3-gap-fill" style="vertical-align: 0px; font-size: 32px;"></i></button>
-            <button type="button" class="tewi-btn-icon"><i class="bi bi-grid-3x3-gap-fill" style="vertical-align: 0px; font-size: 32px;"></i></button>
+            <div class="d-flex gap-2">
+                <button type="button" class="btn btn-primary" style="width: 235px; height: 38px;"><i class="bi bi-feather"></i> Register Pet</button>
+                <button type="button" class="btn btn-secondary" style="width: 150px; height: 38px;"><i class="bi bi-dice-5"></i> Random</button>
             </div>
         </div>
     </nav>
     <main class="p-3">
         <div class="iagd-containers">
-            <div class="row g-3"> <!-- Added gutter spacing -->
-                <div class="col-lg-3 col-md-3 col-sm-3"> <!-- Proper column sizing -->
-                    <div class="card h-100"> <!-- Added h-100 for equal height -->
+            <div class="row g-3">
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <div class="card card-hoverable h-100">
                         <div class="row g-0">
                             <div class="col-md-4">
-                                <img src="{{ asset('images/iagd-container-dog-raw.jpeg') }}" class="img-fluid rounded-start" alt="...">
+                                <img src="{{ asset('images/iagd-container-dog-raw.jpeg') }}" class="img-fluid w-100 h-100 object-fit-cover rounded-start" alt="...">
                             </div>
                             <div class="col-md-8">
                                 <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Content goes here...</p>
-                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    <b class="card-title">Dogs</b>
+                                    <p class="card-text">25,034&nbsp;registered</p>
+                                    <p class="card-text"><small class="text-muted">Newest 3 mins ago</small></p>
+                                </div>
+                                <div class="card-icon">
+                                    <i class="bi bi-caret-right-fill"></i>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-3"> <!-- Proper column sizing -->
-                    <div class="card h-100"> <!-- Added h-100 for equal height -->
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <div class="card card-hoverable h-100">
                         <div class="row g-0">
                             <div class="col-md-4">
-                                <img src="..." class="img-fluid rounded-start" alt="...">
+                                <img src="{{ asset('images/iagd-container-cat-raw-2.jfif') }}" class="img-fluid w-100 h-100 object-fit-cover rounded-start" alt="...">
                             </div>
                             <div class="col-md-8">
                                 <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Content goes here...</p>
-                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    <b class="card-title">Cats</b>
+                                    <p class="card-text">8,034&nbsp;registered</p>
+                                    <p class="card-text"><small class="text-muted">Newest 3 mins ago</small></p>
+                                </div>
+                                <div class="card-icon">
+                                    <i class="bi bi-caret-right-fill"></i>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-3"> <!-- Proper column sizing -->
-                    <div class="card h-100"> <!-- Added h-100 for equal height -->
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <div class="card card-hoverable h-100">
                         <div class="row g-0">
                             <div class="col-md-4">
-                                <img src="..." class="img-fluid rounded-start" alt="...">
+                                <img src="{{ asset('images/iagd-container-dog-raw.jpeg') }}" class="img-fluid w-100 h-100 object-fit-cover rounded-start" alt="...">
                             </div>
                             <div class="col-md-8">
                                 <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Content goes here...</p>
-                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    <b class="card-title">Birds</b>
+                                    <p class="card-text">34&nbsp;registered</p>
+                                    <p class="card-text"><small class="text-muted">Newest 3 mins ago</small></p>
+                                </div>
+                                <div class="card-icon">
+                                    <i class="bi bi-caret-right-fill"></i>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-3"> <!-- Proper column sizing -->
-                    <div class="card h-100"> <!-- Added h-100 for equal height -->
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <div class="card card-hoverable h-100">
                         <div class="row g-0">
                             <div class="col-md-4">
-                                <img src="..." class="img-fluid rounded-start" alt="...">
+                                <img src="{{ asset('images/iagd-container-dog-raw.jpeg') }}" class="img-fluid w-100 h-100 object-fit-cover rounded-start" alt="...">
                             </div>
                             <div class="col-md-8">
                                 <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Content goes here...</p>
-                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    <b class="card-title">Rabbits</b>
+                                    <p class="card-text">4&nbsp;registered</p>
+                                    <p class="card-text"><small class="text-muted">Newest 3 mins ago</small></p>
+                                </div>
+                                <div class="card-icon">
+                                    <i class="bi bi-caret-right-fill"></i>
                                 </div>
                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-1 g-3">
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <div class="card card-hoverable h-100">
+                        <img src="{{ asset('images/iagd-container-dog-raw.jpeg') }}" class="img-fluid w-100 object-fit-cover rounded-start" alt="..." style="max-height: 256px;">
+                        <div class="card-body">
+                            <b class="card-title">Animal Facilities</b>
+                            <p class="card-text">534&nbsp;registered</p>
+                            <p class="card-text"><small class="text-muted">Newest 3 mins ago</small></p>
+                        </div>
+                        <div class="card-icon" style="top: 250px;">
+                            <i class="bi bi-caret-right-fill"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-12">
+                    <div class="card card-hoverable h-100">
+                        <img src="{{ asset('images/iagd-container-cat-raw.png') }}" class="img-fluid w-100 object-fit-cover rounded-start" alt="..." style="height: 256px;">
+                        <div class="card-body">
+                            <b class="card-title">Vet & Medical</b>
+                            <p class="card-text">534&nbsp;records</p>
+                            <p class="card-text"><small class="text-muted">Newest 3 mins ago</small></p>
+                        </div>
+                        <div class="card-icon" style="top: 250px;">
+                            <i class="bi bi-caret-right-fill"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-12">
+                    <div class="card card-hoverable h-100">
+                        <img src="{{ asset('') }}" class="img-fluid w-100 object-fit-cover rounded-start" alt="..." style="height: 256px;">
+                        <div class="card-body">
+                            <b class="card-title">Other Animals</b>
+                            <p class="card-text">534&nbsp;registered</p>
+                            <p class="card-text"><small class="text-muted">Newest 3 mins ago</small></p>
+                        </div>
+                        <div class="card-icon" style="top: 250px;">
+                            <i class="bi bi-caret-right-fill"></i>
                         </div>
                     </div>
                 </div>
@@ -115,7 +167,12 @@
 </body>
 <script>
 $(document).ready(function() {
-
+    $('.card-hoverable').on('mouseover', function() {
+        $(this).find('.card-title').addClass('text-gradient-primary');
+    });
+    $('.card-hoverable').on('mouseout', function() {
+        $(this).find('.card-title').removeClass('text-gradient-primary');
+    });
 });
 </script>
 </html>
