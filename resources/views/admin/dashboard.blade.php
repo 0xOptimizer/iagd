98e5d4d92ad0939494114be9bc181cd65e1914dd
Mel <romelcub@gmail.com>
2025-03-31 15:01:14 +0800

    Updates : Mon, Mar 31, 2025  3:01:13 PM - Fix css location

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    @include('admin.sections.metatags')
+
+    @include('admin.sections.stylesheets')
+
+</head>
+
+<body id="admin-page">
+
+    <div class="wrapper">
+
+    </div>
+
+    @include('jsVars')
+
+    @include('admin.sections.jsscripts')
+
+</body>
+
+</html>
