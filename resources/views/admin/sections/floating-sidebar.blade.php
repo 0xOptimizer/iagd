e94485ddd704e3d0a62079d9cd40b319cdeb17a7
Mel <romelcub@gmail.com>
2025-04-03 15:55:47 +0800

    Update : Thu, Apr  3, 2025  3:55:46 PM - Update sidebar , main js , styles

--- resources/views/admin/sections/floating-sidebar.blade.php
+++ resources/views/admin/sections/floating-sidebar.blade.php
@@ -55,12 +55,61 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link link-dark">
-                        <svg class="bi me-2" width="16" height="16">
-                            <use xlink:href="#people-circle"></use>
-                        </svg>
-                        Profile
+                    <a href="#settingsCollapse"
+                        class="nav-link link-dark d-flex flex-row justify-content-start align-items-center"
+                        data-bs-toggle="collapse" href="">
+                        <div class="svg-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                            </svg>
+                        </div>
+                        <span class="ms-2">Settings</span>
                     </a>
+                    <div id="settingsCollapse" class="collapse multi-collapse ms-3">
+                        <ul class="nav d-flex flex-column justify-content-center ">
+                            <li class="nav-item">
+                                <a class="nav-link link-dark d-flex flex-row justify-content-start align-items-center" href="#">
+                                    <div class="svg-icon-xs me-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                                          </svg>
+
+                                    </div>
+                                    Account
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link link-dark d-flex flex-row justify-content-start align-items-center" href="#">
+                                    <div class="svg-icon-xs me-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                                          </svg>
+
+                                    </div>
+                                    Web
+                                </a>
+                            </li>
+
+
+
+                            <li class="nav-item">
+                                <a class="btnLogoutAdmin nav-link link-dark d-flex flex-row justify-content-start align-items-center" href="javascript::void(0);">
+                                    <div class="svg-icon-xs me-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                                          </svg>
+
+                                    </div>
+                                    Logout
+                                </a>
+                            </li>
+                        </ul>
+
+                    </div>
                 </li>
 
             </ul>
