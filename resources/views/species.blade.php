a6901fb17b224c9e11d7d986e34a1b2d1b6abe08
ビビ <38562978+0xOptimizer@users.noreply.github.com>
2025-04-13 18:06:39 +0800

    hotfix: species list card background

--- resources/views/species.blade.php
+++ resources/views/species.blade.php
@@ -123,20 +123,20 @@ function fetchSpecies(page = 1, startsWith = 'a', species = 'cats') {
             const createdAt = pet.details?.created_at ? moment(pet.details.created_at).fromNow() : 'Unknown';
 
             const card = `
-            <div class="col-lg-3 col-md-3 col-sm-12">
-                <div class="card card-hoverable h-100" style="${isFirstTime ? 'display: none;' : ''}; position: relative;">
-                    <div class="card-background" style="position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-image: url('${image}'); background-size: cover; background-position: center; filter: blur(8px); z-index: -1;"></div>
-                    <img src="${image}" class="img-fluid w-100 rounded-start" alt="${name}'s Cute Photo" style="height: 256px; object-fit: contain; object-position: top;">
-                    <div class="card-body">
-                        <b class="card-title">${name}</b>
-                        <p class="card-text">${iagdNumber}</p>
-                        <p class="card-text"><small class="text-muted">Newest ${createdAt}</small></p>
+                <div class="col-lg-3 col-md-3 col-sm-12">
+                    <div class="card card-hoverable h-100" style="${isFirstTime ? 'display: none;' : ''}; position: relative;">
+                        <div class="card-background" style="position: absolute; top: 0; left: 0; right: 0; height: 256px; background-image: url('${image}'); background-size: cover; background-position: center; filter: blur(8px); z-index: -1;"></div>
+                        <img src="${image}" class="img-fluid w-100 rounded-start" alt="${name}'s Cute Photo" style="height: 256px; object-fit: contain; object-position: top;">
+                        <div class="card-body">
+                            <b class="card-title">${name}</b>
+                            <p class="card-text">${iagdNumber}</p>
+                            <p class="card-text"><small class="text-muted">Newest ${createdAt}</small></p>
+                        </div>
+                        <div class="card-icon" style="top: 250px;">
+                            <i class="bi bi-caret-right-fill"></i>
+                        </div>
                     </div>
-                    <div class="card-icon" style="top: 250px;">
-                        <i class="bi bi-caret-right-fill"></i>
-                    </div>
-                </div>
-            </div>`;
+                </div>`;
             $('.species-list-container').append(card);
         });
         $('.species-list-records-count').text(response.total_species_count);
